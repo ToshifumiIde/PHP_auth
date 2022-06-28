0b114416ba3bbcedb9e884023fdc13f5d05265a6
井手 俊文 <toshifumiide198765@gmail.com>
2022-06-28 15:39:05 +0900

    httpリクエスト・レスポンス

---
+++ response/redirect.php
@@ -0,0 +1,8 @@
+<?php declare(strict_types=1);
+
+header('HTTP/1.1 307 Temporary Redirect');
+header('Location: http://localhost:8888/20220517/contact/contact.html');
+?>
+<body>
+  別のサイトにリダイレクトします。
+</body>
